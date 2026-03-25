8b22f96de186a9f20c15a9316dd836f7fd77a076
Renato Teixeira <renato.gfteixeira@outlook.com>
2026-03-25 11:30:09 -0300

    feat: add dynamic document titles to AuthPage and DashboardPage using Helmet

--- src/modules/auth/pages/AuthPage.tsx
+++ src/modules/auth/pages/AuthPage.tsx
@@ -1,8 +1,10 @@
 import { useLogin } from "@/modules/auth/hooks";
+import { getPageTitle } from "@/shared/utils";
 import { LogIn } from "lucide-react";
-import { type SyntheticEvent, useEffect, useState } from "react";
+import { type SyntheticEvent, useState } from "react";
 import { Button } from "@/shared/components/Button";
 import { InputField } from "@/shared/components/InputField";
+import { Helmet } from "react-helmet-async";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export function AuthPage() {
@@ -19,49 +21,51 @@ export function AuthPage() {
     navigate(from, { replace: true });
   };
 
-  useEffect(() => {
-    document.title = "HS System | Login";
-  }, []);
-
   return (
-    <main className="bg-background min-h-screen px-4">
-      <div className="container mx-auto flex h-screen max-w-4xl flex-col items-center justify-center gap-2">
-        <img src="/favicon.svg" alt="Henna Software logo icon" className="size-32 opacity-80" />
+    <>
+      <Helmet>
+        <title>{getPageTitle("Login")}</title>
+      </Helmet>
 
-        <form onSubmit={handleLogin} className="bg-card flex w-full max-w-md flex-col gap-4 rounded-lg p-6 shadow-lg">
-          <InputField
-            placeholder={"Email"}
-            label={"Email"}
-            showLabel={false}
-            type={"email"}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required={true}
-          />
+      <main className="bg-background min-h-screen px-4">
+        <div className="container mx-auto flex h-screen max-w-4xl flex-col items-center justify-center gap-2">
+          <img src="/favicon.svg" alt="Henna Software logo icon" className="size-32 opacity-80" />
 
-          <InputField
-            placeholder={"Password"}
-            label={"Password"}
-            showLabel={false}
-            type={"password"}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required={true}
-          />
+          <form onSubmit={handleLogin} className="bg-card flex w-full max-w-md flex-col gap-4 rounded-lg p-6 shadow-lg">
+            <InputField
+              placeholder={"Email"}
+              label={"Email"}
+              showLabel={false}
+              type={"email"}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required={true}
+            />
 
-          <hr className="border-card-foreground border opacity-5" />
+            <InputField
+              placeholder={"Password"}
+              label={"Password"}
+              showLabel={false}
+              type={"password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required={true}
+            />
 
-          <Button
-            type={"submit"}
-            isLoading={loading}
-            disabled={loading}
-            loadingText={"Signing in"}
-            rightAddon={<LogIn size={20} />}
-          >
-            Sign In
-          </Button>
-        </form>
-      </div>
-    </main>
+            <hr className="border-card-foreground border opacity-5" />
+
+            <Button
+              type={"submit"}
+              isLoading={loading}
+              disabled={loading}
+              loadingText={"Signing in"}
+              rightAddon={<LogIn size={20} />}
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </main>
+    </>
   );
 }
