13c8e7236bd1cc199c4beacef07998c9d0caea36
Renato Teixeira <renato.gfteixeira@outlook.com>
2026-03-25 11:29:39 -0300

    feat: wrap app with HelmetProvider to enable document head management

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { AuthProvider } from "@/modules/auth/components";
-import { AppRoute } from "@/router";
+import { HelmetProvider } from "react-helmet-async";
 import { ToastProvider } from "@/shared/components/Toast";
+import { AuthProvider } from "@/modules/auth/components";
 import { BrowserRouter } from "react-router-dom";
+import { AppRoute } from "@/router";
 
 export default function App() {
   return (
-    <ToastProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <AppRoute />
-        </BrowserRouter>
-      </AuthProvider>
-    </ToastProvider>
+    <HelmetProvider>
+      <ToastProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <AppRoute />
+          </BrowserRouter>
+        </AuthProvider>
+      </ToastProvider>
+    </HelmetProvider>
   );
 }
