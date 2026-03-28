c760a4ae625a785a2de146221c64b4acd1156956
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2026-03-28 09:40:37 -0300

    refactor: remove deprecated StatsCard and StatsGrid components

--- src/modules/dashboard/components/StatsGrid.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-type Props = {
-  children: ReactNode;
-};
-
-export default function StatsGrid({ children }: Props) {
-  return <div className="grid min-w-0 grid-cols-1 gap-6 *:min-w-0 sm:grid-cols-2 xl:grid-cols-4">{children}</div>;
-}
