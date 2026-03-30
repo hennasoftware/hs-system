e034bdaf17cef1de10f083c702362cc9297154fc
Renato Teixeira <renato.gfteixeira@outlook.com>
2026-03-29 23:34:33 -0300

    feat: add Order type, index export, and initial OrdersPage structure with table and actions

--- src/modules/orders/pages/OrdersPage.tsx
+++ src/modules/orders/pages/OrdersPage.tsx
@@ -1,10 +1,23 @@
-import { useAuth } from "@/modules/auth/hooks";
+import { Button } from "@/shared/components/Button";
+import { PenBox, Plus, Trash } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import type { Order } from "../types";
+import { Table } from "@/shared/components/Table";
 import { getPageTitle } from "@/shared/utils";
 import { Helmet } from "react-helmet-async";
 import { SidebarMenu } from "@/shared/components/Sidebar";
 
+const orders: Order[] = [
+  // TODO: Implement function to gather "orders" from database
+];
+
 export function OrdersPage() {
-  const { user } = useAuth();
+  const navigate = useNavigate();
+  const tableCols = [
+    { key: "id", header: "Order ID", accessor: (o: Order) => o.id, sortable: true },
+    { key: "clientId", header: "Client ID", accessor: (o: Order) => o.clientId },
+    { key: "createdAt", header: "Created At", accessor: (o: Order) => o.createdAt, sortable: true },
+  ];
 
   return (
     <>
@@ -13,18 +26,59 @@ export function OrdersPage() {
       </Helmet>
 
       <SidebarMenu>
-        <main className="bg-background flex min-h-screen flex-col items-center justify-center px-6 text-center lg:px-8">
-          <p className="text-primary text-base font-semibold">
-            Hello, {user?.firstName} {user?.lastName}!
-          </p>
+        <main className="min-h-screen">
+          <div className="mx-auto w-full">
+            <div className="mb-12">
+              <h1 className="text-2xl font-semibold tracking-tight">Orders</h1>
 
-          <h1 className="text-foreground mt-4 text-5xl font-semibold tracking-tight text-balance sm:text-7xl">
-            Orders
-          </h1>
+              <p className="text-muted-foreground text-sm">
+                Track and manage all orders efficiently. Browse existing orders, create new ones, and edit or delete
+                records to keep everything up to date.
+              </p>
+            </div>
 
-          <p className="text-muted-foreground mt-6 text-lg font-medium text-pretty sm:text-xl/8">
-            There's nothing here yet.
-          </p>
+            <div className="mb-6 flex w-full items-center justify-end">
+              <div className="max-w-40">
+                <Button
+                  type={"button"}
+                  leftAddon={<Plus size={20} />}
+                  onClick={() => navigate("/orders/new")}
+                  // TODO: implement "/orders/new" route to add new orders
+                >
+                  New Order
+                </Button>
+              </div>
+            </div>
+
+            <Table<Order>
+              columns={tableCols}
+              data={orders}
+              context={"orders"}
+              searchable
+              defaultPageSize={10}
+              actions={(order) => (
+                <div className="flex gap-2">
+                  <button
+                    className="text-primary hover:text-primary-hover hover:cursor-pointer"
+                    onClick={() => navigate("/orders/o/:id")}
+                    // TODO: implement "/orders/o/:id" route to edit orders
+                  >
+                    <PenBox size={16} />
+                  </button>
+
+                  <button
+                    className="text-destructive hover:text-destructive-hover hover:cursor-pointer"
+                    onClick={() => {
+                      console.log("Delete", order.id);
+                    }}
+                    // TODO: implement "Delete order" function to delete orders
+                  >
+                    <Trash size={16} />
+                  </button>
+                </div>
+              )}
+            />
+          </div>
         </main>
       </SidebarMenu>
     </>
---
+++ src/modules/orders/types/Order.types.ts
@@ -0,0 +1,7 @@
+export interface Order {
+  id: string;
+  clientId: string;
+  createdAt: string;
+  updatedAt: string;
+  [key: string]: string;
+}
