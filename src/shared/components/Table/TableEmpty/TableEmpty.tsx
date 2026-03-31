fef011c4d32bad32237399075429526b77e92e0b
Renato Teixeira <renato.gfteixeira@outlook.com>
2026-03-30 22:39:15 -0300

    refactor: update table components for improved empty state and search input UX

--- src/shared/components/Table/TableEmpty/TableEmpty.tsx
+++ src/shared/components/Table/TableEmpty/TableEmpty.tsx
@@ -1,9 +1,12 @@
+import { MessageSquareX } from "lucide-react";
 import type { TableEmptyProps } from "./TableEmpty.types";
 
 export function TableEmpty({ context, emptyState }: TableEmptyProps) {
   return (
-    <div className="mt-60 flex flex-col items-center gap-4 text-center">
-      <p className="text-muted-foreground text-center font-medium">
+    <div className="text-placeholder mt-40 flex flex-col items-center gap-4 text-center">
+      <MessageSquareX />
+
+      <p className="text-center font-medium">
         {emptyState ?? (context ? `No ${context} found` : "No registers found")}
       </p>
     </div>
