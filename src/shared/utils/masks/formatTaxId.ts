312d1d18a23654a83f08fc421324cdc3d6ee1f26
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2026-04-21 13:32:10 -0300

    feat: add formatters for taxId and zipCode inputs

---
+++ src/shared/utils/masks/formatTaxId.ts
@@ -0,0 +1,8 @@
+export function formatTaxId(value: string) {
+  const numbers = value.replace(/\D/g, "").slice(0, 11);
+
+  return numbers
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+}
